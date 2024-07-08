a4d6a2b0fa1a6a1459cf1cbc3762204518378471
muhammedfahad <muhammedfahadat2003@gmail.com>
2024-07-08 23:09:12 +0530

    created server and mongodb connection

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+const mongoDB = require("./Config/Config");
 const cors = require("cors");
 const port = 5000;
 
 app.use(cors());
 
 app.use(express.json());
+
+
+// server will only start if the mongoDB connection is successful //
+mongoDB().then(() => {
+ try {
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+ } catch (error) {
+    console.log(error, "Server failed to start");
+ }
+});
 
