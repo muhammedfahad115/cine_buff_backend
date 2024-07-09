b03441ca97b9879dbfa968901ec9da90fbcc7102
muhammedfahad <muhammedfahadat2003@gmail.com>
2024-07-10 00:47:24 +0530

    created verifyToken as middleware for authentication

---
+++ Middleware/VerifyToken.js
@@ -0,0 +1,18 @@
+const Jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.token;
+    console.log(authHeader);
+    if (authHeader) {
+        const token = authHeader.split(" ")[1];
+        Jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+            if (err) res.status(403).json("Token is not valid!");
+            req.user = user;
+            next();
+        });
+    } else {
+        return res.status(401).json("You are not authenticated!");
+    }
+};
+
+module.exports = verifyToken;
--- Routes/Routes.js
+++ Routes/Routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { SignUp, Login } = require("../Controllers/UserControllers");
 const router = express.Router();
+const verifyToken = require("../Middleware/VerifyToken");
 
 router.post('/signup',SignUp);
 router.post ('/login',Login);
